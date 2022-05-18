e7fe78e104668ae79e7204988943619b5a6dd576
alexeysharandin <Immolation01>
2022-05-18 15:27:25 +0530

    first commit for Quarkus JDBC Sqlite Driver

---
+++ runtime/src/main/java/io/quarkiverse/jdbc/sqlite/runtime/SqliteAgroalConnectionConfigurer.java
@@ -0,0 +1,23 @@
+package io.quarkiverse.jdbc.sqlite.runtime;
+
+import io.agroal.api.configuration.supplier.AgroalDataSourceConfigurationSupplier;
+import io.quarkus.agroal.runtime.AgroalConnectionConfigurer;
+
+/**
+ * Emply Agoral configurer
+ *
+ * @author Alexey Sharandin <sanders@yandex.ru>
+ */
+public class SqliteAgroalConnectionConfigurer implements AgroalConnectionConfigurer {
+
+    @Override
+    public void disableSslSupport(String databaseKind, AgroalDataSourceConfigurationSupplier dataSourceConfiguration) {
+        // do not log anything for H2
+    }
+
+    @Override
+    public void setExceptionSorter(String databaseKind, AgroalDataSourceConfigurationSupplier dataSourceConfiguration) {
+        // Do not log a warning: we don't have an exception sorter for H2,
+        // but there is nothing the user can do about it.
+    }
+}
